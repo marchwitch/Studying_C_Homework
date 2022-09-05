0ad63df864e6e74aee6b4328becc0eee6f152a3f
Nadezhda Koroleva <marchwitch@gmail.com>
2022-09-05 19:07:56 +0300

    add task 6 for seminar 1

---
+++ Sem1_Task006_Even_number/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = number%2;
+
+if (result == 0)
+{
+    Console.WriteLine("чётное");
+}
+else
+{ 
+    Console.WriteLine("нечётное");
+}
