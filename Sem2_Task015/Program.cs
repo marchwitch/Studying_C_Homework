fc1557f23783be615eb92b64d1a929517eb87496
Nadezhda Koroleva <marchwitch@gmail.com>
2022-09-06 21:44:56 +0300

    add task 015 (bool) for seminar 2

---
+++ Sem2_Task015/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.WriteLine("Введите число от 1 до 7: ");
+int DayOfWeek = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(CheckDay(DayOfWeek));
+
+bool CheckDay(int num)
+{
+    return num == 6 || num == 7;
+}
