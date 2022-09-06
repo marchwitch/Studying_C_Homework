debb9f399bcb0e9e8c86e54412b09191665659ed
Nadezhda Koroleva <marchwitch@gmail.com>
2022-09-06 21:42:17 +0300

    add task 010 for seminar 2

---
+++ Sem2_Task010/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число
+// и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Введите трёхзначное число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+int ShowSecondDigit(int num)
+{
+        int digit1 = num % 100;
+        int digit2 = digit1 / 10;
+        return digit2;
+}
+
+Console.WriteLine(ShowSecondDigit(number1));
