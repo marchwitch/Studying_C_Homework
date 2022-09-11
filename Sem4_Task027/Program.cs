41719472ef09498b1b5eaa18eda3cbb9811cd1e7
Nadezhda Koroleva <marchwitch@gmail.com>
2022-09-11 13:40:04 +0300

    added task 27 for sem4

---
+++ Sem4_Task027/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 2: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11; 82 -> 10; 9012 -> 12
+
+int Prompt (string message)
+{
+    Console.Write(message);
+    int answer = Convert.ToInt32(Console.ReadLine());
+    return answer;
+}
+
+int SumOfDigits(int number)
+{
+    int sum = 0;
+    
+    if(number < 0) number = - number;
+
+    while (number > 0)
+    {
+        int remain = number % 10;
+        number = number / 10;
+        sum += remain;
+    }
+    return sum;
+}
+
+int num = Prompt("Введите число > ");
+Console.WriteLine($"Сумма цифр в числе равна {SumOfDigits(num)}");
