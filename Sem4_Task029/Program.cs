b9cde368d252c5c6d4df6ae0ec111488b35783e1
Nadezhda Koroleva <marchwitch@gmail.com>
2022-09-11 13:40:28 +0300

    added task 29 for sem4

---
+++ Sem4_Task029/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 29: Напишите программу, которая выводит массив из 8 элементов, заполненный случайными числами. 
+// Оформите заполнение массива и вывод в виде функции.
+
+int[] FillArray()
+{
+    int[] array = new int[8];
+    for (int i = 0; i < array.Length; i++)
+    {
+      array[i] = new Random().Next(0, 100);  
+    }
+    return array;
+}
+
+void ShowArray(int[] numbers)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write($"{numbers[i]} ");
+    }
+}
+
+int[] collection = FillArray();
+ShowArray(collection);
