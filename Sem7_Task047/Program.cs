e7e4260a8750a58c56d3a2a4f19314464707227b
Nadezhda Koroleva <marchwitch@gmail.com>
2022-09-27 09:23:28 +0300

    added task 47 for sem7

---
+++ Sem7_Task047/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+void FillArray(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = Math.Round(new Random().NextDouble() * 10, 2);
+        }
+    }
+}
+
+void PrintArray(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i,j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int rows = new Random().Next(3,6);
+int columns = new Random().Next(3,6);
+double[,] array = new double[rows,columns];
+
+FillArray(array);
+PrintArray(array);
