e1568ecb012fdedf5eae6d870585d5120af09fd2
Nadezhda Koroleva <marchwitch@gmail.com>
2022-10-04 15:12:44 +0300

    added task 66 for sem9

---
+++ Sem9_Task066/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.WriteLine("Введите число M: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Сумма натуральных элементов в промежутке от M до N:");
+Console.WriteLine(SumNumbersFromMToN(number1, number2));
+
+int SumNumbersFromMToN(int start, int end)
+{
+    if (start == end)
+    {
+        return start;
+    }
+    return(start + SumNumbersFromMToN(start + 1, end));
+}
